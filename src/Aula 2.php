866386d8e4ae19785c873604a39a050fa2eace5f
Thiago <titimkt@gmail.com>
2021-04-13 20:44:43 -0300

    teste

---
+++ src/Aula 2.php
@@ -0,0 +1,14 @@
+<?php
+
+echo date("d/m/Y"); // 13/04/2021
+
+/*
+o comando acima imprime a data atual
+echo - imprimir
+date - data atual 
+"d/m/Y" - formato da data
+*/
+
+function ObterDataAtual() {
+    return date("d/m/Y");
+ }
