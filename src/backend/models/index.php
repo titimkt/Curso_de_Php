ceadbcfadca81cdfb92c80613ab8f9ccb87d2bca
Thiago <titimkt@gmail.com>
2021-05-18 21:52:54 -0300

    transferindo a tabela pro JavaScript

--- src/backend/models/index.php
+++ src/backend/models/index.php
@@ -1,17 +1,17 @@
 <?php
 
 $dados = [
-     "Nome" => "Thiago",
-     "Sobrenome" => "Sacchett",
-     "Idade" => 36,
-     "Endereço" => [
-          "Rua" => "Ezílio Michelin",
-          "Número" => "115",
-          "Complemento" => "apto 507",
-          "Bairro" => "Humaitá",
-          "CEP" => "95705-050",
-          "Cidade" => "Bento Gonçalves",
-     ]
+   "Nome" => "Thiago",
+   "Sobrenome" => "Sacchett",
+   "Idade" => 36,
+   "Endereço" => [
+      "Rua" => "Ezílio Michelin",
+      "Número" => "115",
+      "Complemento" => "apto 507",
+      "Bairro" => "Humaitá",
+      "CEP" => "95705-050",
+      "Cidade" => "Bento Gonçalves",
+   ]
 ];
 
 $dia = 30; // tipo de número inteiro
@@ -21,9 +21,10 @@ $salario = 30.25; // tipo de número real (real, float, double, longFloat, )
 
 echo json_encode($dados);
 
-class pessoa {
-     public $Nome = "";
-     public $Sobrenome = "";
-     public $Idade = "";
-     private $Endereco = "Teste";
+class pessoa
+{
+   public $Nome = "";
+   public $Sobrenome = "";
+   public $Idade = "";
+   private $Endereco = "Teste";
 }
--- src/frontend/js/tabela.js
+++ src/frontend/js/tabela.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() {
+/*
+$(document).ready(function() {
      console.log( "ready!" );
 
      $('.btn-criar-usuario').on('click', function(){
@@ -27,4 +28,77 @@ function ValidarForm () {
        })
    }
  });
+*/
 
+$(document).ready(function() {
+
+   var TabelaUsuarios = $("#IdTabelaUsuarios");
+   MontarTabela()
+   
+   function MontarTabela() {
+      var Cabecalho = MontarCabecalhoTabela();
+      var Corpo = MontarCorpoTabela();
+      var HtmlDaTabela = (Cabecalho + Corpo);
+
+      TabelaUsuarios.html(HtmlDaTabela);
+
+   }
+
+   function MontarCabecalhoTabela() {
+      var Cabecalho = (
+         '<thead class="table-dark">' +
+            '<tr>' +
+               '<th scope="col">ID</th>' +
+               '<th scope="col">Nome</th>' +
+               '<th scope="col">Sobrenome</th>' +
+               '<th scope="col">Usuário</th>' +
+               '<th scope="col">Cidade</th>' +
+               '<th scope="col">Estado</th>' +
+               '<th scope="col">Ações</th>' +
+            '</tr>' +
+         '</thead>'
+      );
+
+      return Cabecalho;
+
+   }
+
+   function MontarCorpoTabela() {
+      var Corpo = (
+         '<tbody>' +
+            '<tr>' +
+               '<td>1</td>' +
+               '<td>Mark</td>' +
+               '<td>Otto</td>' +
+               '<td>@mdo</td>' +
+               '<td>Garibaldi</td>' +
+               '<td>Rio Grande do Sul</td>' +
+               '<td><button type="button" class="btn btn-outline-warning">Editar</button>&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-outline-danger">Excluir</button>' +
+               '</td>' +
+            '</tr>' +
+            '<tr>' +
+               '<td>2</td>' +
+               '<td>Jacob</td>' +
+               '<td>Thornton</td>' +
+               '<td>@fat</td>' +
+               '<td>Curitiba</td>' +
+               '<td>Paraná</td>' +
+               '<td><button type="button" class="btn btn-outline-warning">Editar</button>&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-outline-danger">Excluir</button>' +
+            '</tr>' +
+            '<tr>' +
+               '<td>3</td>' +
+               '<td>Larry</td>' +
+               '<td>the Bird</td>' +
+               '<td>@twitter</td>' +
+               '<td>Penha</td>' +
+               '<td>Santa Catarina</td>' +
+               '<td><button type="button" class="btn btn-outline-warning">Editar</button>&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-outline-danger">Excluir</button>' +
+            '</tr>' +
+         '</tbody>'
+      );
+
+      return Corpo;
+
+   }
+
+});
